24bb357fbc73a3342b55914d47c40d8cf833daae
Biao <biao.boey@gmail.com>
2017-11-07 16:40:41 -0800

    First commit

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Biao on 10/31/17.
+//  Copyright © 2017 Biao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
